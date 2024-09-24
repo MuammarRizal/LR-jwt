e8da2537d92506a68ccc1cff276d7d8c62a9f802
MuammarRizal <muammarrizal31@gmail.com>
2024-09-25 03:59:08 +0700

    auth jwt

--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -24,13 +24,46 @@ const Dashboard: React.FC = () => {
       setName(decoded.name);
       setExpires(decoded.exp);
     } catch (error: any) {
-      console.log(error);
+      if (error.response) {
+        document.location.href = "/login";
+      }
     }
   };
 
+  const axiosJWT = axios.create();
+
+  axiosJWT.interceptors.request.use(
+    async (config) => {
+      const currentDate = new Date();
+      if (parseInt(expires) * 1000 < currentDate.getTime()) {
+        const response = await axios.get("http://localhost:4000/token");
+        config.headers.Authorization = `Bearer ${response.data.accessToken}`;
+        setToken(response.data.accessToken);
+
+        const decoded = jwtDecode<JwtDecodedCustom>(response.data.accessToken);
+        setName(decoded.name);
+        setExpires(decoded.exp);
+      }
+      return config;
+    },
+    (err) => {
+      return Promise.reject(err);
+    }
+  );
+
+  const getUsers = async () => {
+    console.log(expires);
+    const response = await axiosJWT.get("http://localhost:4000", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(response.data.data);
+  };
   return (
     <div className="container">
       <h2>Hello {name}</h2>
+      <button onClick={getUsers}>Get Users</button>
     </div>
   );
 };
